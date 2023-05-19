ad9ca9e854deafffdf177f697c8c2bb707d6a39d
Hatt3rPi <fabarca212@gmail.com>
2023-05-18 20:42:43 -0400

    trazabilidad

--- ayun/plantkeeper/plantas/modifica_plantas.php
+++ ayun/plantkeeper/plantas/modifica_plantas.php
@@ -2,6 +2,27 @@
 session_start();
 include '/home/gestio10/procedimientos_almacenados/config_ayun.php';
 
+function trazabilidad($link, $usuario, $archivo, $descripcion, $id_modificado, $query) {
+    mysqli_set_charset($link, 'utf8');
+    // Prepara la consulta SQL
+    $stmt_2 = mysqli_prepare($link, "INSERT INTO trazabilidad_query(usuario, archivo, descripcion, id_modificado, query) VALUES (?, ?, ?, ?, ?)");
+    // Verifica si la preparación de la consulta fue exitosa
+    if ($stmt_2 === false) {
+        die("ERROR: No se pudo preparar la consulta SQL. " . mysqli_error($link));
+    }
+    // Vincula los parámetros a la consulta SQL
+    mysqli_stmt_bind_param($stmt_2, "sssis", $usuario, $archivo, $descripcion, $id_modificado, $query);
+
+    // Ejecuta la consulta
+    if (mysqli_stmt_execute($stmt_2)) {
+        echo "Datos de trazabilidad añadidos con éxito.";
+    } else {
+        echo "ERROR: No se pudo ejecutar la consulta. " . mysqli_error($link);
+    }
+
+    // Cierra la declaración
+    mysqli_stmt_close($stmt_2);
+}
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['csrf_token']==$_SESSION['csrf_token']) {
     switch($_POST['accion_bbdd']){
         case 'ingreso':
@@ -13,15 +34,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['csrf_token']==$_SESSION['csr
 
             $sql = "INSERT INTO plantas (especie, ubicacion, humedad_sustrato_minima, humedad_sustrato_maxima, tamano) VALUES (?, ?, ?, ?, ?)";
 
-            $stmt = mysqli_prepare($link, $sql);
-
+            $stmt_2 = mysqli_prepare($link, $sql);
+            
             if ($stmt === false) {
                 echo "ERROR: No se pudo preparar la consulta SQL. " . mysqli_error($link);
             } else {
                 mysqli_stmt_bind_param($stmt, "ssiii", $especie, $ubicacion, $humedad_sustrato_minima, $humedad_sustrato_maxima, $tamano);
 
                 if (mysqli_stmt_execute($stmt)) {
-                    echo "Planta añadida con éxito.";
+                    trazabilidad($link, $_SESSION['usuario'], basename(__FILE__), 'ingresa planta', 0, "INSERT INTO plantas (especie, ubicacion, humedad_sustrato_minima, humedad_sustrato_maxima, tamano) VALUES ('".$especie."', '".$ubicacion."', ". $humedad_sustrato_minima.", ".$humedad_sustrato_maxima.", ".$tamano.")") 
+
                 } else {
                     echo "ERROR: No se pudo ejecutar la consulta. " . mysqli_error($link);
                 }
@@ -32,6 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['csrf_token']==$_SESSION['csr
         case 'eliminacion':
             $id = intval($_POST['id']);
             $sql = "DELETE FROM plantas WHERE id = ?";
+            trazabilidad($link, $_SESSION['usuario'], basename(__FILE__), 'elimina planta', $id, $sql.$id)
             $stmt = mysqli_prepare($link, $sql);
             if ($stmt === false) {
                 echo "ERROR: No se pudo preparar la consulta SQL. " . mysqli_error($link);
