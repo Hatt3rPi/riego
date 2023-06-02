f6f11ab3ccadf1af367ab45ee08602313cb6d6ef
Hatt3rPi <fabarca212@gmail.com>
2023-06-01 23:29:34 -0400

    fix

--- ayun/plantkeeper/sensores/relaciones_plantas.php
+++ ayun/plantkeeper/sensores/relaciones_plantas.php
@@ -3,13 +3,13 @@
 include '/home/gestio10/procedimientos_almacenados/config_ayun.php';
 header('Content-Type: application/json');
 
-$sql =  "SELECT a.id, a.especie, a.ubicacion, " +
-        "MAX(CASE WHEN b.tipo_sensor = 'humedad_sustrato' THEN b.pin ELSE NULL END) AS humedad_sustrato," +
-        "MAX(CASE WHEN b.tipo_sensor = 'bomba_agua' THEN b.pin ELSE NULL END) AS bomba_agua" +
-        "FROM plantas as a " +
-        "LEFT JOIN recolectores_pines as b " +
-        "ON a.id = b.id_planta" +
-        "GROUP BY " +
+$sql =  "SELECT a.id, a.especie, a.ubicacion, " .
+        "MAX(CASE WHEN b.tipo_sensor = 'humedad_sustrato' THEN b.pin ELSE NULL END) AS humedad_sustrato," .
+        "MAX(CASE WHEN b.tipo_sensor = 'bomba_agua' THEN b.pin ELSE NULL END) AS bomba_agua " .
+        "FROM plantas as a " .
+        "LEFT JOIN recolectores_pines as b " .
+        "ON a.id = b.id_planta " .
+        "GROUP BY " .
         "a.id, a.especie, a.ubicacion";
 $result = mysqli_query($link, $sql);
 
